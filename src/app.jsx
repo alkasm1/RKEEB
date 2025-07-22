209df3530ef63e6fec42ab856a57b52c794761e0
alkasm1 <shamwebs1@gmail.com>
2025-07-22 09:42:02 +0300

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import CaptureZone from "./components/CaptureZone.jsx";
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <h1>🕵️‍♂️ RKEEB | كشف التزوير البصري</h1>
+      <CaptureZone />
+    </div>
+  );
+}
+
+export default App;
