126cdff908eb52a6578b57839661c6e4de060291
alkasm1 <shamwebs1@gmail.com>
2025-07-22 12:31:41 +0300

    Delete src/utils/similarity.js

--- src/utils/similarity.js
+++
@@ -1,8 +0,0 @@
-export function calculateSimilarity(hash1, hash2) {
-  const minLength = Math.min(hash1.length, hash2.length);
-  let diff = 0;
-  for (let i = 0; i < minLength; i++) {
-    if (hash1[i] !== hash2[i]) diff++;
-  }
-  return Math.round(((minLength - diff) / minLength) * 100);
-}
