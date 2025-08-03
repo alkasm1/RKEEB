861736b7d3217b2b85150162eb51253602e64bf8
رأفت عمر <shamwebs1@gmail.com>
2025-08-03 14:53:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/RKEEB/',
+  base: '/',
+  plugins: [react()],
   build: {
-    outDir: 'docs'
+    outDir: 'dist'
   }
 });
